a3ee9dfc0153af3cdccd905c44e5774e81eb7944
farhanfarruq <anakpcd987@gmail.com>
2025-09-07 04:13:59 +0700

    update deploy

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -27,7 +27,7 @@ export const AuthProvider = ({ children }) => {
         setIsLoggingIn(true);
         setError(null);
         try {
-            const response = await fetch('http://127.0.0.1:8000/api/login', {
+                const response = await fetch(`${import.meta.env.VITE_API_URL}/api/login`, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' },
                 body: JSON.stringify({ username, password }),
