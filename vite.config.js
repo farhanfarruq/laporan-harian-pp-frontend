27791cf37b582dc59bed78371e15356b2fd24663
farhanfarruq <anakpcd987@gmail.com>
2025-09-13 09:08:28 +0700

    update minggu

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path' // Impor modul 'path' bawaan Node.js
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // Definisikan alias '@' untuk menunjuk ke direktori '/src'
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
