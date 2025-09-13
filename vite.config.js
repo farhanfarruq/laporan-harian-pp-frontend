38f83d33fa47c991e453ead6bc112327c5940686
farhanfarruq <anakpcd987@gmail.com>
2025-09-13 09:14:05 +0700

    taek

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Tambahkan blok 'resolve' ini untuk mendefinisikan alias
-  resolve: {
-    alias: {
-      // Ini memberitahu Vite bahwa '@' adalah shortcut untuk direktori '/src'
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
 })
-
